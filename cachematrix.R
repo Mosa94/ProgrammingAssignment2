b525e963c2589bcc5265cb3e818016c21c5222de
Gabriele Mosaico <gabriele.mosaico@icloud.com>
2018-05-28 16:58:17 +0200

    from mean to solve

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,34 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+# inizializing the functions used in cacheSolve
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
-
+#if already solved, the function returns the cached matrix
+#otherwise solves and returns the solved matrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached matrix")
+    return(s)
+  }
+  mat <- x$get()
+  s <- solve(mat, ...)
+  x$setsolve(s)
+  s
 }
